1582a56b3029a1c8f3d1e7bac6729b3196911de9
Simon Zeng <simon.zeng@fnz.com>
2025-09-21 11:14:08 +0800

    update

---
+++ src/bin/0005.rs
@@ -0,0 +1,4 @@
+// cargo add sqlx@0.8 --features runtime-tokio-rustls,postgres,uuid,chrono,migrate
+// cargo add uuid@1 --features v4,serde
+// cargo add chrono@0.4 --features serde
+// cargo add dotenvy@0.15
